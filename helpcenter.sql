84469e7ce91595e13309e29f5da142d0e4a6eddc
Juanes522 <117606505+Juanes522@users.noreply.github.com>
2023-09-25 18:46:14 -0500

    Uploaded database called helpcenter.sql
    The second version of the database was created, taking account the creation of the four different tables for person, participants, psychologist and general services. and the two important atributes.

---
+++ helpcenter.sql
@@ -0,0 +1,54 @@
+CREATE DATABASE helpcenter;
+
+USE helpcenter;
+
+CREATE TABLE persons(
+id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(100) UNIQUE NOT NULL,
+userpassword VARCHAR(100) UNIQUE NOT NULL,
+personname VARCHAR(100) NOT NULL,
+document MEDIUMINT UNIQUE NOT NULL,
+dateofbirth DATE NOT NULL,
+cityofbirth VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE participants(
+id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(100) UNIQUE NOT NULL,
+userpassword VARCHAR(100) UNIQUE NOT NULL,
+personname VARCHAR(100) NOT NULL,
+document MEDIUMINT UNIQUE NOT NULL,
+dateofbirth DATE NOT NULL,
+cityofbirth VARCHAR(100) NOT NULL,
+participatedsessions INT NOT NULL,
+nickname VARCHAR(100) NOT NULL
+
+);
+
+CREATE TABLE psychologist(
+id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(100) UNIQUE NOT NULL,
+userpassword VARCHAR(100) UNIQUE NOT NULL,
+personname VARCHAR(100) NOT NULL,
+document MEDIUMINT UNIQUE NOT NULL,
+dateofbirth DATE NOT NULL,
+cityofbirth VARCHAR(100) NOT NULL,
+yearofgraduation YEAR NOT NULL,
+daysinservice INT NOT NULL,
+suportedsessions INT NOT NULL,
+salary DOUBLE NOT NULL
+
+);
+
+CREATE TABLE generalservice(
+id INT PRIMARY KEY AUTO_INCREMENT,
+username VARCHAR(100) UNIQUE NOT NULL,
+userpassword VARCHAR(100) UNIQUE NOT NULL,
+personname VARCHAR(100) NOT NULL,
+document MEDIUMINT UNIQUE NOT NULL,
+dateofbirth DATE NOT NULL,
+cityofbirth VARCHAR(100) NOT NULL,
+salary DOUBLE NOT NULL,
+sessionscleaned INT NOT NULL
+
+);
